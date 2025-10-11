4df73ef3439f6df6f97f8607f962feff8ade44ae
Sai Tanush <saitanush789@gmail.com>
2025-10-11 19:56:12 +0530

    feat: updated prmptVersion type

--- src/interfaces/IPromptVersion.ts
+++ src/interfaces/IPromptVersion.ts
@@ -1,10 +1,16 @@
 import { Document, Types } from 'mongoose';
-
 export interface IPromptVersion {
-    promptId: Types.ObjectId; 
-    content: string; 
-    versionNumber: number;
-    createdAt: Date;
+  promptId: Types.ObjectId; 
+
+  event: 'create' | 'update' | 'delete';
+
+  beforeObject?: Record<string, any> | null;
+
+  afterObject?: Record<string, any> | null;
+
+  versionNumber: number;
+
+  createdAt: Date;
 }
 
 export interface IPromptVersionDocument extends IPromptVersion, Document {}
