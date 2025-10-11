154dc32c5ba1ff367f9fa2b656264e809c210f69
Sai Tanush <saitanush789@gmail.com>
2025-10-11 13:22:40 +0530

    feat: added User interface

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,14 @@
+import { Document } from 'mongoose';
+
+export interface IUser {
+    username: string;
+    email: string;
+    password?: string; 
+    role: 'standard' | 'admin';
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface IUserDocument extends IUser, Document {
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
