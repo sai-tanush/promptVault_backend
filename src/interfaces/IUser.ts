00d36f4bce4b7c52691fd47171bbe319f07ad349
Sai Tanush <saitanush789@gmail.com>
2025-10-11 14:22:11 +0530

    feat: added first_name in model

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -2,6 +2,7 @@ import { Document } from 'mongoose';
 
 export interface IUser {
     username: string;
+	first_name: string;
     email: string;
     password?: string; 
     role: 'standard' | 'admin';
