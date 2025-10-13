cd979f5051e776387988b2adda09b85503511561
Sai Tanush <saitanush789@gmail.com>
2025-10-13 18:05:32 +0530

    feat: updated port

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGODB_URL!)
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
