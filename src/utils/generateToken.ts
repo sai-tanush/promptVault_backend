50a9baef2fbac84d57cbb2c1e596e363b67c0e2e
Sai Tanush <saitanush789@gmail.com>
2025-10-13 15:01:41 +0530

    feat: updated error ahndling for secret

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,6 +2,10 @@ import jwt, { Secret } from 'jsonwebtoken';
 
 export const generateToken = (id: string) => {
 	const secret = process.env.JWT_SECRET as Secret;
+
+	if (!secret) {
+		throw new Error('JWT_SECRET is not set in environment variables');
+	}
   	const expiresIn = process.env.JWT_EXPIRES_IN || '7d';
 
 	return jwt.sign({ id }, secret, { expiresIn: expiresIn as any });
