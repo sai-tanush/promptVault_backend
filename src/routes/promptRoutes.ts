e672edbe6c05e0b54d385046288bae056d0bd55e
Sai Tanush <saitanush789@gmail.com>
2025-10-14 18:35:02 +0530

    feat: added route for getPromptWithVersions in promptRoutes

--- src/routes/promptRoutes.ts
+++ src/routes/promptRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { archivePrompt, createPrompt, deletePrompt, getPrompts, updatePrompt } from "../controllers/promptController";
+import { archivePrompt, createPrompt, deletePrompt, getPrompts, getPromptWithVersions, updatePrompt } from "../controllers/promptController";
 import { isAuthUser } from "../middlewares/isAuthUser";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/update/:id", isAuthUser, updatePrompt);
 router.delete("/prompt/:id", isAuthUser, deletePrompt)
 router.patch("/archieve/:id", isAuthUser, archivePrompt);
 router.get("/getprompts", isAuthUser, getPrompts);
+router.get("/prompt/:id", isAuthUser, getPromptWithVersions);
 
 export default router;
