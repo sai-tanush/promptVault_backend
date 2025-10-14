a9a63ce0f40d179b53a33ce36effc30b311c0c4c
Sai Tanush <saitanush789@gmail.com>
2025-10-14 17:43:20 +0530

    feat: added route for deleting prompt

--- src/routes/promptRoutes.ts
+++ src/routes/promptRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPrompt, updatePrompt } from "../controllers/promptController";
+import { createPrompt, deletePrompt, updatePrompt } from "../controllers/promptController";
 import { isAuthUser } from "../middlewares/isAuthUser";
 
 const router = express.Router();
 
 router.post("/create", isAuthUser, createPrompt);
 router.post("/update/:id", isAuthUser, updatePrompt);
+router.delete("/prompt/:id", isAuthUser, deletePrompt)
 
 export default router;
