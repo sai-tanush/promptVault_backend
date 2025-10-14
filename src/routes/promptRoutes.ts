6a60b8ed7e48efe333d12feea5026cb298edab62
Sai Tanush <saitanush789@gmail.com>
2025-10-14 16:18:08 +0530

    feat: create new prompt route to update prompt

--- src/routes/promptRoutes.ts
+++ src/routes/promptRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPrompt } from "../controllers/promptController";
+import { createPrompt, updatePrompt } from "../controllers/promptController";
 import { isAuthUser } from "../middlewares/isAuthUser";
 
 const router = express.Router();
 
 router.post("/create", isAuthUser, createPrompt);
+router.post("/update/:id", isAuthUser, updatePrompt);
 
 export default router;
