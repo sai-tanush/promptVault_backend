6300bf1e3b8dd647af2a7cfeb55016b542167599
Sai Tanush <saitanush789@gmail.com>
2025-10-14 18:41:12 +0530

    feat: added route for restorePrompt in promptRoutes

--- src/routes/promptRoutes.ts
+++ src/routes/promptRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { archivePrompt, createPrompt, deletePrompt, getPrompts, getPromptWithVersions, updatePrompt } from "../controllers/promptController";
+import { archivePrompt, createPrompt, deletePrompt, getPrompts, getPromptWithVersions, restorePrompt, updatePrompt } from "../controllers/promptController";
 import { isAuthUser } from "../middlewares/isAuthUser";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post("/create", isAuthUser, createPrompt);
 router.post("/update/:id", isAuthUser, updatePrompt);
 router.delete("/prompt/:id", isAuthUser, deletePrompt)
 router.patch("/archieve/:id", isAuthUser, archivePrompt);
+router.patch("/restore/:id", isAuthUser, restorePrompt);
 router.get("/getprompts", isAuthUser, getPrompts);
 router.get("/prompt/:id", isAuthUser, getPromptWithVersions);
 
