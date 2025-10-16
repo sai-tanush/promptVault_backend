a0c931adb89c06fd953cb827661f7cff262fdede
Sai Tanush <saitanush789@gmail.com>
2025-10-17 00:29:09 +0530

    feat: updated route names

--- src/routes/promptRoutes.ts
+++ src/routes/promptRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express";
-import { archivePrompt, createPrompt, deletePrompt, getPrompts, getPromptWithVersions, restorePrompt, updatePrompt } from "../controllers/promptController";
+import { archivePrompt, createPrompt, deletePrompt, restorePrompt, updatePrompt, getPromptWithLatestVersion, getPromptWithAllVersions, getAllUserPrompts } from "../controllers/promptController";
 import { isAuthUser } from "../middlewares/isAuthUser";
 
 const router = express.Router();
 
-router.post("/create", isAuthUser, createPrompt);
-router.post("/update/:id", isAuthUser, updatePrompt);
-router.delete("/prompt/:id", isAuthUser, deletePrompt)
-router.patch("/archieve/:id", isAuthUser, archivePrompt);
-router.patch("/restore/:id", isAuthUser, restorePrompt);
-router.get("/getprompts", isAuthUser, getPrompts);
-router.get("/prompt/:id", isAuthUser, getPromptWithVersions);
+router.post("/", isAuthUser, createPrompt);
+router.patch("/:promptId", isAuthUser, updatePrompt);
+router.delete("/:promptId", isAuthUser, deletePrompt)
+router.patch("/:promptId/archive", isAuthUser, archivePrompt);
+router.patch("/:promptId/restore", isAuthUser, restorePrompt);
+router.post("/", isAuthUser, getAllUserPrompts)
+router.get("/:promptId/latest", isAuthUser, getPromptWithLatestVersion);
+router.get("/:promptId/versions", isAuthUser, getPromptWithAllVersions);
 
 export default router;
